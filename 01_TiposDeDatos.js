7765086b7512e4019493b5a6006e35ecf8849ba0
Mateo Solano <mateosolano65@gmail.com>
2023-08-27 16:37:42 -0500

    First commit

---
+++ 01_TiposDeDatos.js
@@ -0,0 +1,23 @@
+// Tipos de datos
+
+//Numericos - number
+
+console.log(5);
+console.log(15.5);
+
+//Objeto
+console.log({ nombre: "Juan", edad: 18 });
+
+// lista-array
+console.log([
+  "Colombia",
+  "Peru",
+  2,
+  4,
+  4,
+  5,
+  5,
+  7,
+  { nombre: "Mateo" },
+  ["a", "b", "c", "d"],
+]);
---
+++ 02_Variables.js
@@ -0,0 +1,54 @@
+var ciudad;
+console.log(ciudad);
+
+
+var nombre = 12;
+console.log(nombre);
+
+var persona = {
+    nombre: 'Mateo',
+    apellido: 'Solano',
+    edad: 17,
+    direccion: {
+        calle: 133,
+        numero: '157A',
+    },
+    deportesFav: [
+        'Futbol',
+        'Ciclismo',
+    ]
+}
+console.log(persona);
+
+var ciudad = 'rddsf';
+ciudad = 'venez';
+console.log(ciudad);
+
+// Variables Let
+
+let nombre = 'Mateo';
+console.log(nombre);
+
+{
+    let saludo = 'Holassssss'
+    console.log(saludo);
+}
+console.log(saludo);
+
+{
+    var saludo = 'Holassssss'
+    console.log(saludo);
+}
+console.log(saludo);
+
+// constante
+
+const PI = 3.14;
+console.log(PI);
+
+
+
+
+var nombre = 'Mateo';
+var saludo = `Hola soy ${nombre}`;
+console.log(saludo);
