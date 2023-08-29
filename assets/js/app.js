86244243f9769fcac3d4a22319f4883e5690c04e
Mateo Solano <mateosolano65@gmail.com>
2023-08-29 11:19:45 -0500

    Agregamos orden (assets)

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+console.log('Hola mundo');
+
+let a = 10,
+    b = 80,
+    c = 'Hola ',
+    d = 'Spiderman!',
+    x = c + d ;
+    
+    const saludo = c + d;
+
+console.log('%c Mis variables', 'color:blue; font-weight: bold' );
+console.log({a});
+console.log({b});
+console.log({c});
+
+console.info(c);
+console.error(c);
+
+console.table({a, b, c, d, x});
