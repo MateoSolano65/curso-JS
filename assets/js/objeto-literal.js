15f67529bf954d8d64af3c626bd5e1b4ae120814
Mateo Solano <mateosolano65@gmail.com>
2023-08-31 16:00:13 -0500

    Objetos literales mitad

---
+++ assets/js/objeto-literal.js
@@ -0,0 +1,16 @@
+let personaje = {
+    nombre: 'Mateo Solano',
+    id: 1,
+    vivo: false,
+    edad: 17,
+    direccion: {
+        calle: 133,
+        numero: '154A',
+        casa: 47,
+    },
+    vestimenta: true,
+
+};
+
+console.log(personaje);
+console.log('Nombre:', personaje.nombre);
