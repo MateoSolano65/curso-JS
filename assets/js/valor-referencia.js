5d7a5362f3a4aff1d78a9878511576b699ea0c30
Mateo Solano <mateosolano65@gmail.com>
2023-09-12 10:20:24 -0500

    Hasta aquí llega el if-else

--- assets/js/valor-referencia.js
+++ assets/js/valor-referencia.js
@@ -7,15 +7,15 @@ console.log({a,b});
 
 
 let juan = { nombre: 'Juan'};
-// EXPRED: separa los elementos {...juan}
-let ana = juan;
+// SPREAD: separa los elementos {...juan}
+let ana = {...juan};
 ana.nombre = 'Ana';
 
 console.log({juan,ana});
 
 
 // REST: Une todos los argumentos en una sola variable cuando es parentesis (...persona)
-const cambiarNombre = ( persona ) => {
+const cambiarNombre = ( {...persona} ) => {
     persona.nombre = 'Tony'
     return persona
 };
@@ -24,3 +24,24 @@ let mateo = { nombre: 'Mateo'};
 let tony = cambiarNombre(mateo);
 
 console.log({mateo,tony});
+
+//Arreglos
+
+const frutas = ['Pera','Banano'];
+
+
+//.slice es para romper la relacion PERO SPREAD SIEMPRE SERÁ MÁS RAPIDO
+
+console.time('slice'); 
+const otrasFrutas = frutas.slice();
+console.timeEnd('slice'); 
+
+console.time('spread'); 
+const otrasFrutas2 = [...frutas];
+console.timeEnd('spread'); 
+
+
+
+otrasFrutas.push('Fresa');
+
+console.log({frutas, otrasFrutas});
